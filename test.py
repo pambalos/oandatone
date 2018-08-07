e430252383b4ae089d54fb3a957c27521328d0c0
pambalos <bradleyjusticeca@gmail.com>
2018-08-06 21:40:37 -0700

    slight modifications from earlier

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import configparser #ConfigParser to connect to oanda
-import pandas as pd #pandas
-import oandapy as opy
-import numpy as np
+import pandas as pd #pandas module
+import oandapy as opy #oanda rest API wrapper
+import numpy as np #numpy module for data analytics
 
-sets = {('2018-7-23', '2018-7-27')}
+sets = {('2018-7-23', '2018-7-25')} #date sets
 
 config = configparser.ConfigParser()
 config.read('oanda.cfg')
@@ -14,7 +14,7 @@ for pair in sets:
     st = pair[0]
     nd = pair[1]
     dataOne = oanda.get_history(instrument = 'EUR_USD', start = st, end = nd, granularity = 'M1')
-
+    dataTwo = oanda.get_history(instrument = 'USD_CAD', start = st, end = nd, granularity = 'M1')
 
 #dataOne = oanda.get_history(instrument = 'EUR_USD', start='2018-7-25', end='2018-7-27', granularity = 'M1')
 #dataTwo = oanda.get_history(instrument = 'USD_CAD', start='2018-7-25', end='2018-7-27', granularity = 'M1')
@@ -26,3 +26,5 @@ frameOne.info()
 frameTwo = pd.DataFrame(dataTwo['candles']).set_index('time')
 frameTwo.index = pd.DatetimeIndex(frameTwo.index)
 frameTwo.info()
+
+frameOne['returns'] = np.log(frameOne['closeAsk'])
